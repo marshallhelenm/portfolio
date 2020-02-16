135b35d33747ef74136687441fed8f709ffa1555
marshallhelenm <marshallhelenm@gmail.com>
2020-02-16 11:03:43 -0800

    using postmans syntax

--- src/index.js
+++ src/index.js
@@ -4,18 +4,24 @@ function main() {
 }
 
 function fetchPosts() {
-  console.log('fetching posts')
-  fetch("https://dev.to/api/articles/me/published", {
+  console.log("fetching posts");
+
+  var myHeaders = new Headers();
+  myHeaders.append("api-key", "kVaN98morrQBgw3sQDSR3dqK");
+
+  var requestOptions = {
     method: "GET",
-    headers: {
-      "api-key": "kVaN98morrQBgw3sQDSR3dqK"
-    }
-  })
-    .then(response => response.json())
-    .then(posts => {
-      console.log(posts);
-    });
+    headers: myHeaders,
+    redirect: "follow"
+  };
+
+  fetch("https://dev.to/api/articles/me/published?=", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log("error", error));
 }
+
+// https://dev.to/feed/marshallhelenm
 
 // <article class="col-6 col-12-xsmall work-item">
 // 								<a href="https://dev.to/marshallhelenm/creating-the-tome-part-ii-hdo" class="image fit thumb"><img src="https://res.cloudinary.com/practicaldev/image/fetch/s--cFDANO1F--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://res.cloudinary.com/dwfqeeh5f/image/upload/v1575140146/Blog%2520Images/Screen_Shot_2019-11-30_at_10.38.43_AM.png" alt="" /></a>
